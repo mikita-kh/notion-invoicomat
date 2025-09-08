114679595b0d6ff45d092ad5c4263ae887774dd7
mikita.khatsimtsou <mikita.khatsimtsou@flyrlabs.com>
2025-09-08 17:15:09 +0200

    :recycle: Refactor ConfigModule and SecretManagerService for improved secret management and initialization

--- functions/src/app/secret-manager/secret-manager.module.ts
+++ functions/src/app/secret-manager/secret-manager.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
-import { FirebaseModule } from '../firebase/firebase.module'
 import { SecretManagerService } from './secret-manager.service'
 
 @Module({
-  imports: [FirebaseModule],
   providers: [SecretManagerService],
   exports: [SecretManagerService],
 })
